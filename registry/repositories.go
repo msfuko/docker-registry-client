a53944601317202b82e1db1bd39b7e69aaeda431
Geoff Baskwill <Geoff_Baskwill@trendmicro.com>
2018-03-06 14:27:24 -0500

    Support streaming repository listing
    Added StreamRepositories(ctx context.Context) (<-chan string, <-chan error)
    that will stream the repositories on the returned channel until done or the
    context is Done().

--- registry/repositories.go
+++ registry/repositories.go
@@ -1,5 +1,9 @@
 package registry
 
+import (
+	"context"
+)
+
 type repositoriesResponse struct {
 	Repositories []string `json:"repositories"`
 }
@@ -23,4 +27,58 @@ func (registry *Registry) Repositories() ([]string, error) {
 			return nil, err
 		}
 	}
+}
+
+func (registry *Registry) StreamRepositories(ctx context.Context) (<-chan string, <-chan error) {
+	regChan := make(chan string)
+	errChan := make(chan error)
+
+	go func() {
+		// defer close(errChan)
+		defer close(regChan)
+
+		url := registry.url("/v2/_catalog")
+
+		var err error //We create this here, otherwise url will be rescoped with :=
+		var response repositoriesResponse
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				registry.Logf("registry.repositories url=%s", url)
+				url, err = registry.getPaginatedJson(url, &response)
+				switch err {
+				case ErrNoMorePages:
+					if !registry.streamPage(ctx, regChan, response.Repositories) {
+						return
+					}
+					return
+				case nil:
+					if !registry.streamPage(ctx, regChan, response.Repositories) {
+						return
+					}
+					continue
+				default:
+					errChan <- err
+					return
+				}
+			}
+		}
+	}()
+
+	return regChan, errChan
+}
+
+func (registry *Registry) streamPage(ctx context.Context, c chan string, v []string) bool {
+	for _, r := range v {
+		select {
+		case <-ctx.Done():
+			return false
+		case c <- r:
+			// next
+		}
+	}
+	return true
 }
